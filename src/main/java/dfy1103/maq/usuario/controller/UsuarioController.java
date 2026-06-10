d8129808964062b6cd109166b03de0015d412792
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-10 09:33:28 -0400

    agregando swagger. falta hateoas

--- src/main/java/dfy1103/maq/usuario/controller/UsuarioController.java
+++ src/main/java/dfy1103/maq/usuario/controller/UsuarioController.java
@@ -2,7 +2,14 @@ package dfy1103.maq.usuario.controller;
 
 import dfy1103.maq.usuario.dto.UsuarioRequestDTO;
 import dfy1103.maq.usuario.dto.UsuarioResponseDTO;
+import dfy1103.maq.usuario.model.Usuario;
 import dfy1103.maq.usuario.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,25 +20,49 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/bibliotecaam/usuario")
 @RequiredArgsConstructor
+@Tag(name = "Usuarios", description = "Operaciones asociadas a usuarios.")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos los usuarios", description = "Obtiene una lista de todos los usuarios.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operacion exitosa"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     public ResponseEntity<List<UsuarioResponseDTO>> obtenerTodos(){
         return ResponseEntity.ok(usuarioService.obtenerTodos());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener usuario por id.", description = "Obtiene una lista de todos los usuarios.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operacion exitosa"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Long id){
         return usuarioService.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
+    @Operation(summary = "Guardar un usuario", description = "Guarda un usuario acorde a lo ingresado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa."),
+            @ApiResponse(responseCode = "400", description = "Error al ingresar parametros. Revise si ingreso todos los parametros solicitados."),
+            @ApiResponse(responseCode = "403", description = "No tienes permiso para hacer el cambio.")
+    })
     public ResponseEntity<UsuarioResponseDTO> guardar(@Valid @RequestBody UsuarioRequestDTO doto){
         return ResponseEntity.status(201).body(usuarioService.guardar(doto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar usuario", description = "Actualiza un usuario acorde a una id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario actualizado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "404", description = "El id del usuario no existe.")
+    })
     public ResponseEntity<UsuarioResponseDTO> actualizar(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO doto){
         return usuarioService.actualizar(id, doto)
                 .map(ResponseEntity::ok)
@@ -39,6 +70,11 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar usuario", description = "Elimina un usuario acorde a una id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Usuario eliminado con exito!"),
+            @ApiResponse(responseCode = "404",description = "ERROR: ¡El id del usuario ingresado no existe!")
+    })
     public ResponseEntity<Void> eliminar(@PathVariable Long id){
         if (usuarioService.buscarPorId(id).isEmpty()){
             return ResponseEntity.notFound().build();
